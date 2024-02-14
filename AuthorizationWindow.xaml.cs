9b754ea0991c338656723c67526af44e78a955ea
Vladislav Otdelyanov <vlad_1995@mail.ru>
2024-02-14 19:51:58 +0300

    Add project files.

---
+++ AuthorizationWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RNP24_5_1
+{
+    /// <summary>
+    /// Interaction logic for AuthorizationWindow.xaml
+    /// </summary>
+    public partial class AuthorizationWindow : Window
+    {
+        public AuthorizationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (LoginTb.Text == "Usr" && PasswordPb.Password == "123")
+            {
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Пользователя с такими данными не существует!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+
+                LoginTb.Clear();
+                PasswordPb.Clear();
+            }
+        }
+
+        private void ExitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
